fb7c380bf40c32131f5b4c5a3eb08847b7abf5e7
SlashGordon <slash.gordon.dev@gmail.com>
2024-10-22 07:47:01 +0200

    updated pattern

--- merge_result.py
+++ merge_result.py
@@ -1,26 +1,33 @@
 import os
 import json
 
+
 def merge_json_files(result_files):
-    merged_results = []
+    merged_results = dict()
 
     for file in result_files:
-        with open(file, 'r') as f:
+        with open(file, "r") as f:
             results = json.load(f)
-            merged_results.extend(results)
+            merged_results.update(results)
 
     return merged_results
 
+
 def main():
     # Get all JSON result files from current directory
-    result_files = [file for file in os.listdir('.') if file.startswith('result_') and file.endswith('.json')]
+    result_files = [
+        os.path.join("results", file)
+        for file in os.listdir("results")
+        if file.startswith("result_") and file.endswith(".json")
+    ]
 
     # Merge the JSON files
     merged_results = merge_json_files(result_files)
 
     # Save the merged results to a new JSON file
-    with open('merged_results.json', 'w') as f:
+    with open("merged_results.json", "w") as f:
         json.dump(merged_results, f, indent=4)
+
 
 if __name__ == "__main__":
     main()
--- optimizer.py
+++ optimizer.py
@@ -1,60 +1,143 @@
 import multiprocessing as mp
 import os
+import math
 
 from pystockfilter.tool.start_chunked_optimizer import StartChunkedOptimizer
 from pystockfilter.tool.start_seq_optimizer import StartSequentialOptimizer as sso
 from pystockfilter.strategy.rsi_strategy import RSIStrategy as rsi
-from pystockfilter.strategy.sma_cross_sma_strategy import SmaCrossSmaStrategy as sma_cross_sma
-from pystockfilter.strategy.ema_cross_ema_strategy import EmaCrossEmaStrategy as ema_cross_ema
-from pystockfilter.strategy.uo_ema_cross_close_strategy import UltimateEmaCrossCloseStrategy as uo_ema_cross_close
-from pystockfilter.strategy.moving_average_rsi_strategy import MovingAverageRSIStrategy as ma_rsi
+from pystockfilter.strategy.sma_cross_sma_strategy import (
+    SmaCrossSmaStrategy as sma_cross_sma,
+)
+from pystockfilter.strategy.ema_cross_ema_strategy import (
+    EmaCrossEmaStrategy as ema_cross_ema,
+)
+from pystockfilter.strategy.uo_ema_cross_close_strategy import (
+    UltimateEmaCrossCloseStrategy as uo_ema_cross_close,
+)
+from pystockfilter.strategy.moving_average_rsi_strategy import (
+    MovingAverageRSIStrategy as ma_rsi,
+)
 from pystockfilter.data.stock_data_source import DataSourceModule as Data
 from pystockfilter.tool.start_chunked_optimizer import StartChunkedOptimizer
 
-CHUNKS = int(os.environ.get("CHUNKS_SIZE", 20)) 
-CHUNKS_IDX = int(os.environ.get("CHUNKS_IDX", 1)) 
+CHUNKS = int(os.environ.get("CHUNKS_SIZE", 20))
+CHUNKS_IDX = int(os.environ.get("CHUNKS_IDX", 1))
 # Set multiprocessing method to "fork" for optimized parallel processing in Unix-based systems.
 mp.set_start_method("fork")
 dax_symbols = [
-    "ADS.F", "AIR.F", "ALV.F", "BAS.F", "BAYN.F", "BMW.F", "BEI.F", "BNR.F", "CON.F", "1COV.F",
-    "DBK.F", "DB1.F", "DHL.F", "DTE.F", "DWNI.F", "EOAN.F", "FME.F", "FRE.F", "HEI.F", "HFG.F",
-    "HEN.F", "IFX.F", "MBG.F", "MRK.F", "MTX.F", "MUV2.F", "PAH3.F", "PUM.F", "QGEN.F", "RWE.F"
+    "ADS.F",
+    "AIR.F",
+    "ALV.F",
+    "BAS.F",
+    "BAYN.F",
+    "BMW.F",
+    "BEI.F",
+    "BNR.F",
+    "CON.F",
+    "1COV.F",
+    "DBK.F",
+    "DB1.F",
+    "DHL.F",
+    "DTE.F",
+    "DWNI.F",
+    "EOAN.F",
+    "FME.F",
+    "FRE.F",
+    "HEI.F",
+    "HFG.F",
+    "HEN.F",
+    "IFX.F",
+    "MBG.F",
+    "MRK.F",
+    "MTX.F",
+    "MUV2.F",
+    "PAH3.F",
+    "PUM.F",
+    "QGEN.F",
+    "RWE.F",
 ]
 
 mda_symbols = [
-  "LHA.F", "ECV.F", "EVK.F", "EVT.F", "FRA.F", "FNTN.F", "FRE.F", 
-  "FPE3.F", "G1A.F", "GXI.F", "HLE.F", "HFG.F", "HAG.F", "HOT.F", 
-  "BOSS.F", "JEN.F", "JUN3.F", "SDF.F", "KGX.F", "KBX.F", "KRN.F", 
-  "LXS.F", "LEG.F", "NEM.F", "NDX1.F", "PUM.F", "RAA.F", "RRTL.F", 
-  "S24.F", "SIL.F", "SAX.F", "TGN.F"
+    "LHA.F",
+    "ECV.F",
+    "EVK.F",
+    "EVT.F",
+    "FRA.F",
+    "FNTN.F",
+    "FRE.F",
+    "FPE3.F",
+    "G1A.F",
+    "GXI.F",
+    "HLE.F",
+    "HFG.F",
+    "HAG.F",
+    "HOT.F",
+    "BOSS.F",
+    "JEN.F",
+    "JUN3.F",
+    "SDF.F",
+    "KGX.F",
+    "KBX.F",
+    "KRN.F",
+    "LXS.F",
+    "LEG.F",
+    "NEM.F",
+    "NDX1.F",
+    "PUM.F",
+    "RAA.F",
+    "RRTL.F",
+    "S24.F",
+    "SIL.F",
+    "SAX.F",
+    "TGN.F",
 ]
 
 sdax_symbols = [
-  "BFSA.F", "GBF.F", "CEV.F", "DHER.F", "FIE.F", "FRA.F", "HLAG.F", 
-  "TKG.F", "HYQ.F", "SKYD.F", "WSU.F", "VZO.F", "VO2.F", "OSR.F", 
-  "BOSS.F", "DUE.F", "A1A.F", "LEG.F", "AIXA.F", "NDA.F", "AT1.F"
+    "BFSA.F",
+    "GBF.F",
+    "CEV.F",
+    "DHER.F",
+    "FIE.F",
+    "FRA.F",
+    "HLAG.F",
+    "TKG.F",
+    "HYQ.F",
+    "SKYD.F",
+    "WSU.F",
+    "VZO.F",
+    "VO2.F",
+    "OSR.F",
+    "BOSS.F",
+    "DUE.F",
+    "A1A.F",
+    "LEG.F",
+    "AIXA.F",
+    "NDA.F",
+    "AT1.F",
 ]
 
 all_symbols = dax_symbols + mda_symbols + sdax_symbols
 
 # Symbols to optimize for the current chunk
-symbols = dax_symbols[len(dax_symbols) // CHUNKS * (CHUNKS_IDX - 1):len(dax_symbols) // CHUNKS * CHUNKS_IDX]
+start_idx = len(all_symbols) // CHUNKS * (CHUNKS_IDX - 1)
+end_idx = len(all_symbols) // CHUNKS * CHUNKS_IDX
+symbols = all_symbols[start_idx:end_idx]
 
 
 parameters = [
     # Strategy: RSI Strategy
-    [   
+    [
         {
             "para_rsi_window": range(5, 25, 1),
         },
         {
             "para_rsi_enter": range(10, 50, 1),
             "para_rsi_exit": range(50, 90, 1),
-            "constraint": lambda p: p.para_rsi_enter < p.para_rsi_exit
-        }
+            "constraint": lambda p: p.para_rsi_enter < p.para_rsi_exit,
+        },
     ],
     # Strategy: Moving average + RSI Strategy
-    [   
+    [
         {
             "para_ma_short": range(2, 20, 1),
             "para_ma_long": range(10, 50, 1),
@@ -63,7 +146,7 @@ parameters = [
         {
             "para_rsi_window": range(5, 30, 1),
             "para_rsi_threshold": range(20, 100, 1),
-        }
+        },
     ],
     # Strategy: Ultimate Oscillator (UO) Strategy with EMA Cross Close
     [
@@ -71,10 +154,8 @@ parameters = [
             "para_uo_upper": range(20, 100, 1),
             "para_uo_lower": range(-30, 50, 1),
             "para_ema_short": 0,
-            "constraint": lambda p: (
-                p.para_uo_upper > p.para_uo_lower
-            ) and (p.para_uo_upper - p.para_uo_lower) > 10,
-            
+            "constraint": lambda p: (p.para_uo_upper > p.para_uo_lower)
+            and (p.para_uo_upper - p.para_uo_lower) > 10,
         },
         {
             "para_uo_short": range(7, 30, 1),
@@ -83,26 +164,22 @@ parameters = [
             "para_ema_short": 0,
             "constraint": lambda p: (
                 p.para_uo_long > p.para_uo_medium > p.para_uo_short
-            ) and (p.para_uo_long - p.para_uo_medium) > 5
-              and (p.para_uo_medium - p.para_uo_short) > 3,
-            
-        }, 
+            )
+            and (p.para_uo_long - p.para_uo_medium) > 5
+            and (p.para_uo_medium - p.para_uo_short) > 3,
+        },
         {
-            "para_ema_short": range(3, 19, 1),            
-        }
+            "para_ema_short": range(3, 19, 1),
+        },
     ],
     # Strategy: SMA Cross SMA Strategy
-    [
-        sma_cross_sma.get_optimizer_parameters()
-    ],
+    [sma_cross_sma.get_optimizer_parameters()],
     # Strategy: EMA Cross EMA Strategy
-    [
-        ema_cross_ema.get_optimizer_parameters()
-    ],
+    [ema_cross_ema.get_optimizer_parameters()],
 ]
 
 my_optimizer = StartChunkedOptimizer(
-    ticker_symbols=dax_symbols[:1],
+    ticker_symbols=symbols,
     strategies=[
         rsi,
         ma_rsi,
@@ -113,9 +190,9 @@ my_optimizer = StartChunkedOptimizer(
     optimizer_class=sso,
     optimizer_parameters=parameters,
     data_source=Data(Data.Y_FINANCE),
-    data_chunk_size=300, 
+    data_chunk_size=300,
 )
 
 results = my_optimizer.run(history_months=30)
 
-results.dump_optimization_results(f"result_{CHUNKS_IDX}.json", True,  True)
+results.dump_optimization_results(f"result_{CHUNKS_IDX}.json", True, True)
